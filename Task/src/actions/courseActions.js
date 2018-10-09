ee03815dfd5083d4119f926a9d17f3339d2d5752
Konstantsin Bulatetsky <bulatetsky.konstantin@gmail.com>
2018-10-09 18:14:04 +0300

    completed 11 - Async Writes in Redux

--- Task/src/actions/courseActions.js
+++ Task/src/actions/courseActions.js
@@ -1,5 +1,38 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course){
-    return {type: types.CREATE_COURSE, course};
+function loadCoursesSuccess(courses){
+    return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+function updateCourseSuccess(course){
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+function createCourseSuccess(course){
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function loadCourses(){
+    return function(dispatch){
+        return courseApi.getAllCourses()
+            .then(courses => dispatch(loadCoursesSuccess(courses)))
+            .catch(error => {
+                throw(error);
+            });
+    };
+}
+
+export function saveCourse(course){
+  return function (dispatch){
+    return courseApi.saveCourse(course)
+      .then(course => {
+        course.id ?
+          dispatch(updateCourseSuccess(course)) :
+          dispatch(createCourseSuccess(course));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
 }
--- Task/src/reducers/courseReducer.js
+++ Task/src/reducers/courseReducer.js
@@ -1,11 +1,25 @@
 import * as actionTypes from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function courseReducer(state = [], action){
-    switch (action.type){
-        case actionTypes.CREATE_COURSE:
-            return [...state, Object.assign({}, action.course)];
-        
-            default:
-                return state;
+export default function courseReducer(state = initialState.courses, action){
+  switch (action.type){
+
+    case actionTypes.LOAD_COURSES_SUCCESS:
+      return action.courses.map(course => Object.assign({}, course));
+
+    case actionTypes.CREATE_COURSE_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.course)
+      ];
+
+    case actionTypes.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
+
+    default:
+      return state;
     }
 }
